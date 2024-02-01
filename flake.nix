857d4f6941816bbccf97c67a940e8e275ab1dce1
Marek Maksimczyk <marek.maksimczyk@mandos.net.pl>
2024-02-01 14:20:34 +0100

    build: Change render system to Hugo
    Basic configuration

--- flake.nix
+++ flake.nix
@@ -3,42 +3,53 @@
 
   outputs = { self, nixpkgs }:
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs { system = "x86_64-linux"; config.allowUnfree = true; };
     in
     {
 
-      packages.x86_64-linux.default =
-        let
-          build-website = pkgs.writeShellApplication
-            {
-              name = "build-website";
-              runtimeInputs = with pkgs; [ emacs29-nox ];
-              text = builtins.readFile ./scripts/build.sh;
-            };
-          publish = pkgs.writeTextFile {
-            name = "emacs-publish";
-            text = builtins.readFile ./scripts/publish.el;
-            destination = "/bin/publish.el";
-          };
-        in
-        pkgs.symlinkJoin {
-          name = "dupa";
-          paths = [ build-website publish ];
-        };
+      # packages.x86_64-linux.default =
+      #   let
+      #     build-website = pkgs.writeShellApplication
+      #       {
+      #         name = "build-website";
+      #         runtimeInputs = with pkgs; [ emacs29-nox ];
+      #         text = builtins.readFile ./scripts/build.sh;
+      #       };
+      #     publish = pkgs.writeTextFile {
+      #       name = "emacs-publish";
+      #       text = builtins.readFile ./scripts/publish.el;
+      #       destination = "/bin/publish.el";
+      #     };
+      #   in
+      #   pkgs.symlinkJoin {
+      #     name = "dupa";
+      #     paths = [ build-website publish ];
+      #   };
 
       devShells.x86_64-linux.default = pkgs.mkShell {
         buildInputs = [
-          self.packages.x86_64-linux.default
+          # self.packages.x86_64-linux.default
           (import ./jekyll/shell.nix { inherit pkgs; })
+          pkgs.hugo
         ];
       };
 
-      # packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
       devShells.x86_64-linux.terraform = pkgs.mkShell {
-        buildInputs = [
+        buildInputs = with pkgs; [
           # pkgs.cdkf
+          nodePackages_latest.cdktf-cli
+          terraform
+          terragrunt
+          terraform-ls
+          go
+          gopls
+          gotools
+          go-tools
+          go-outline
+          gopkgs
+          godef
+          golint
+          watchexec
         ];
       };
 
