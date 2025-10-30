6e45e07368fb346bcea40e8e9e30f74d0b2801e8
Marek Maksimczyk <marek.maksimczyk@mandos.net.pl>
2025-10-30 14:51:33 +0900

    build: Add step to deploy page to S3
    Invalidation of CloudFront is still hardcoded.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env sh
 set -e 
 
-aws s3 sync ./hugo/public s3://mandos.net.pl --delete
+PUBLIC_DIR="$1"
+if [ -z "$PUBLIC_DIR" ]; then
+    PUBLIC_DIR="./hugo/public"
+fi
 
-aws cloudfront create-invalidation --distribution-id E27OAT8VBN5AJ --paths "/*"
+aws s3 sync ./hugo/public s3://mandos.net.pl --delete 
+
+aws cloudfront create-invalidation --distribution-id E27OAT8VBN5AJ --paths "/*" 
