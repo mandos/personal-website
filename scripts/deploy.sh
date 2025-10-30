7e2941f124ae451c39baf6be24df3bcda9610218
Marek Maksimczyk <marek.maksimczyk@mandos.net.pl>
2025-10-30 15:13:04 +0900

    build: Fix wrong public dir in deploy.sh
    Looks like chechout have to bo before dowloading artefacts, also there
    was missing env vars.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
-set -e 
+set -e
 
 PUBLIC_DIR="$1"
 if [ -z "$PUBLIC_DIR" ]; then
     PUBLIC_DIR="./hugo/public"
 fi
 
-aws s3 sync ./hugo/public s3://mandos.net.pl --delete 
+aws s3 sync "$PUBLIC_DIR" s3://mandos.net.pl --delete 
 
 aws cloudfront create-invalidation --distribution-id E27OAT8VBN5AJ --paths "/*" 
